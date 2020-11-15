89b609491bd6d6f18bac353664c2ec0fe105264e
Truong Huy <phuy0212@gmail.com>
2020-11-15 23:10:04 +0700

    Add Save System

---
+++ Assets/Scripts/FrameWork/SaveDataFramework/DataInGame.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public enum DataGameMode
+{
+    Single_Player,
+    Two_Player
+}
+
+[System.Serializable]
+public class DataInGame
+{
+    public List<DataMode> dataGameModes = new List<DataMode>();
+    //[HideInInspector]
+    public string jsonGameMode;
+
+    public void Save()
+    {
+        //for (int i = 0; i < dataGameModes.Count; i++)
+        //{
+        //    dataGameModes[i].Save();
+        //}
+        jsonGameMode = JsonArray.ToJson<DataMode>(dataGameModes.ToArray());
+        PlayerPrefs.SetString("DataInGame", jsonGameMode);
+
+        //DataMode[] dataSaved = JsonArray.FromJson<DataMode>(PlayerPrefs.GetString("DataInGame"));
+        //Debug.Log(dataSaved);
+
+
+    }
+    public void SaveDataMode(DataMode dataMode)
+    {
+        dataMode.Save();
+        Save();
+    }
+
+    public void Load()
+    {
+        Debug.Log("After Load Data :"+ jsonGameMode);
+        dataGameModes.Clear();
+
+
+        if (!string.IsNullOrEmpty(jsonGameMode))
+        {   
+
+            DataMode[] datas = JsonArray.FromJson<DataMode>(jsonGameMode);
+            for (int i = 0; i < datas.Length; i++)
+            {
+                dataGameModes.Add(datas[i]);
+            }
+        }
+        else
+        {
+            foreach (var enumValue in Enum.GetValues(typeof(DataGameMode)))
+            {
+                DataMode mode = new DataMode();
+                mode.dataGameMode = (DataGameMode)enumValue;
+                mode.bestScore = 0;
+                mode.currentScore = 0;
+                dataGameModes.Add(mode);
+            }
+            Save();
+        }
+    }
+
+
+    public DataMode FindDataMode(DataGameMode dataGameMode)
+    {
+        return dataGameModes.Find(x => x.dataGameMode == dataGameMode);
+    }
+}
+
+[System.Serializable]
+public class DataMode
+{
+    public DataGameMode dataGameMode;
+    public int currentScore;
+    public int bestScore;
+
+    public void Save()
+    {
+
+    }
+
+    public void ClearData()
+    {
+        currentScore = 0;
+    }
+
+}
---
+++ Assets/Scripts/FrameWork/SaveDataFramework/GameManager.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private DataInGame dataInGame;
+
+    private DataMode currentDataMode;
+
+    public DataGameMode CurrentDataGameMode = DataGameMode.Single_Player;
+
+    public Text currentModeText;
+    public Text highestScoresText;
+    public Text currentScoresText;
+
+    private int currentScore = 0;
+
+    
+
+    private void Awake()
+    {
+        LoadData();
+        OnGameInitialized();
+
+        //string dataTest = PlayerPrefs.GetString("TestClass", string.Empty);
+
+        //if (!string.IsNullOrEmpty(dataTest))
+        //{
+        //    TestClass t = JsonUtility.FromJson<TestClass>(dataTest);
+
+        //    Debug.Log(t.name + " " + t.score);
+        //}
+
+
+        //TestClass testClass = new TestClass("Huy", 10);
+        //string toJson = JsonUtility.ToJson(testClass);
+
+        //PlayerPrefs.SetString("TestClass", toJson);
+
+    }
+
+    public void Bam()
+    {   
+        currentScoresText.text = "Current Scores : "+(++currentDataMode.currentScore);
+        if (currentDataMode.currentScore > currentDataMode.bestScore)
+            currentDataMode.bestScore = currentDataMode.currentScore;
+        highestScoresText.text = "Highest Scores : " + currentDataMode.bestScore;
+    }
+
+    public void Save()
+    {
+        if (currentDataMode.currentScore > currentDataMode.bestScore)
+            currentDataMode.bestScore = currentDataMode.currentScore;
+
+        currentDataMode.currentScore = 0;
+
+
+        dataInGame.Save();
+
+
+        highestScoresText.text = "Highest Scores : " + currentDataMode.bestScore;
+
+        currentScoresText.text = "Current Scores : " + (currentDataMode.currentScore);
+
+
+    }   
+
+    public void SetGameMode(int indexGameMode)
+    {
+        switch (indexGameMode)
+        {
+            case 0:
+                CurrentDataGameMode = DataGameMode.Single_Player;
+                break;
+            case 1:
+                CurrentDataGameMode = DataGameMode.Two_Player;
+                break;
+        }
+        SetupGameMode();
+    }
+
+    private void SetupGameMode()
+    {
+        switch (CurrentDataGameMode)
+        {
+            case DataGameMode.Single_Player:
+                currentModeText.text = "Current Mode : Single Player";
+                break;
+            case DataGameMode.Two_Player:
+                currentModeText.text = "Current Mode : Two Player";
+                break;
+        }
+
+        currentDataMode = dataInGame.FindDataMode(CurrentDataGameMode);
+
+        highestScoresText.text = "Highest Scores : "+currentDataMode.bestScore;
+    }
+
+    private void OnGameInitialized()
+    {
+        SetupGameMode();
+    }
+
+    
+
+    private void LoadData()
+    {
+        string data = PlayerPrefs.GetString("DataInGame", string.Empty);
+
+        if (string.IsNullOrEmpty(data))
+        {
+            dataInGame.Load();
+        }
+        else
+        {
+            dataInGame.jsonGameMode = data;
+            dataInGame.Load();
+
+        }
+    }
+    
+    private void PlayGame()
+    {
+        currentDataMode = dataInGame.FindDataMode(CurrentDataGameMode);
+    }
+
+    public void SaveDataMode()
+    {
+        dataInGame.SaveDataMode(currentDataMode);
+
+        string data = JsonUtility.ToJson(dataInGame);
+        PlayerPrefs.SetString("DataInGame", data);
+    }
+
+}
+
+public class TestClass
+{
+    public string name;
+    public int score;
+
+    public TestClass(string name, int score)
+    {
+        this.name = name;
+        this.score = score;
+    }
+
+}
